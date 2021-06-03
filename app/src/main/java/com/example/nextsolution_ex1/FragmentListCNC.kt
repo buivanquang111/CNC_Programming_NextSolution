5fa9413a65e6cfc94f1a10fa8207058c1caa997a
buivanquang111 <58690765+buivanquang111@users.noreply.github.com>
2021-06-03 23:45:12 +0700

    update
    update

--- app/src/main/java/com/example/nextsolution_ex1/FragmentListCNC.kt
+++ app/src/main/java/com/example/nextsolution_ex1/FragmentListCNC.kt
@@ -18,12 +18,16 @@ class FragmentListCNC : Fragment() {
     lateinit var rootView: View
     private var listSearch: ArrayList<CNC> = arrayListOf()
     private var list: ArrayList<CNC> = arrayListOf()
+    lateinit var myListItem: Array<String>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         rootView = inflater.inflate(R.layout.fragment_list_cnc, container, false)
+        //create list title item
+        myListItem = resources.getStringArray(R.array.my_list_item)
+        //set list
         setData()
         val adapter: CNCAdapter = CNCAdapter(context, list)
         rootView.recyclerView.layoutManager =
@@ -105,67 +109,12 @@ class FragmentListCNC : Fragment() {
     }
 
     fun setData() {
-        list.add(CNC(0, "[1] What is CNC?", "file:///android_asset/0.html"))
-        list.add(CNC(1, "[2] How to Make CNC Program?", "file:///android_asset/1.html"))
-        list.add(CNC(2, "[3] Make Your First CNC Program", "file:///android_asset/2.html"))
-        list.add(CNC(3, "[4] G Code Introduction", "file:///android_asset/3.html"))
-        list.add(
-            CNC(
-                4,
-                "[5] Modal G-Code- Learn G Code Programming",
-                "file:///android_asset/4.html"
-            )
-        )
-        list.add(
-            CNC(
-                5,
-                "[6] One Shot G-Codes- Learn G Code Programming",
-                "file:///android_asset/5.html"
-            )
-        )
-        list.add(
-            CNC(
-                6,
-                "[7] Mill/Lathe G/M-Codes- Learn G Code Programming",
-                "file:///android_asset/6.html"
-            )
-        )
-        list.add(
-            CNC(
-                7,
-                "[8] Popular CNC G-Code Lists- Learn G Code Programming",
-                "file:///android_asset/7.html"
-            )
-        )
-        list.add(
-            CNC(
-                8,
-                "[9] Din 66025 Programming- Learn G Code Programming",
-                "file:///android_asset/8.html"
-            )
-        )
-        list.add(CNC(9, "[10] M Code Introduction", "file:///android_asset/9.html"))
-        list.add(CNC(10, "[11] CNC Program Block", "file:///android_asset/10.html"))
-        list.add(CNC(11, "[12] Why to use Canned Cycles?", "file:///android_asset/11.html"))
-        list.add(CNC(12, "[13] Subprograms Basics", "file:///android_asset/12.html"))
-        list.add(CNC(13, "[14] CNC Machine Modes", "file:///android_asset/13.html"))
-        list.add(CNC(14, "[15] Speed and Feed Override", "file:///android_asset/14.html"))
-        list.add(CNC(15, "[16] Block Delete", "file:///android_asset/15.html"))
-        list.add(CNC(16, "[17] Dry Run", "file:///android_asset/16.html"))
-        list.add(CNC(17, "[18] Reference Point Return", "file:///android_asset/17.html"))
-        list.add(CNC(18, "[19] CNC Operating Modes", "file:///android_asset/18.html"))
-        list.add(CNC(19, "[20] CNC Reference Point Return", "file:///android_asset/19.html"))
-        list.add(CNC(20, "[21] CNC Speed and Feed Override", "file:///android_asset/20.html"))
-        list.add(CNC(21, "[22] CNC Optional Block Skip", "file:///android_asset/21.html"))
-        list.add(CNC(22, "[23] CNC Dry Run", "file:///android_asset/22.html"))
-        list.add(CNC(23, "[24] How to Set Up CNC Milling?", "file:///android_asset/23.html"))
-        list.add(CNC(24, "[25] Tool Presetter", "file:///android_asset/24.html"))
-        list.add(CNC(25, "[26] Who is CNC Machine Setter?", "file:///android_asset/25.html"))
-        list.add(CNC(26, "[27] Tool Selection (CNC Lathe)", "file:///android_asset/26.html"))
-        list.add(CNC(27, "[28] Main Parts", "file:///android_asset/27.html"))
-        list.add(CNC(28, "[29] Who is CNC Machine Setter?", "file:///android_asset/28.html"))
-        list.add(CNC(29, "[30] Chuck Jaws", "file:///android_asset/29.html"))
-        list.add(CNC(30, "[31] Tool Turret", "file:///android_asset/30.html"))
+        for (i in 0 until myListItem.size){
+            list.add(CNC(i,myListItem[i],getURL(i)))
+        }
+    }
+    fun getURL(index: Int): String{
+        return "file:///android_asset/$index.html"
     }
 
     fun displaySearch(listSearch: ArrayList<CNC>) {
